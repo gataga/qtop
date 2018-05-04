c4560ff2fde3b23e4d355145619c75a4fbe26657
Cas Simons <cassimons@gmail.com>
2018-05-04 16:40:53 +1000

    change regex to fit our PBS

--- qtop_py/plugins/pbs.py
+++ qtop_py/plugins/pbs.py
@@ -7,29 +7,28 @@ import re
 from qtop_py.serialiser import StatExtractor, GenericBatchSystem
 import qtop_py.fileutils as fileutils
 import itertools
-
+import sys
 
 class PBSStatExtractor(StatExtractor):
     def __init__(self, config, options):
+        print "cas was here"
+
         StatExtractor.__init__(self, config, options)
-        self.user_q_search = r'^(?P<host_name>(?P<job_id>[0-9\[\]-]+)\.(?P<domain>[\w-]+))\s+' \
+        # self.user_q_search = r'^(?P<host_name>(?P<job_id>[0-9\[\]-]+)\.(?P<domain>[\w-]+))\s+' \
+        #                      r'(?P<name>[\w%.=+/{}-]+)\s+' \
+        #                      r'(?P<user>[A-Za-z0-9.]+)\s+' \
+        #                      r'(?P<time>\d+:\d+:?\d*|0)\s+' \
+        #                      r'(?P<state>[BCEFHMQRSTUWX])\s+' \
+        #                      r'(?P<queue_name>\w+)'
+
+        self.user_q_search = r'(?P<job_id>\d+)\s+' \
                              r'(?P<name>[\w%.=+/{}-]+)\s+' \
                              r'(?P<user>[A-Za-z0-9.]+)\s+' \
                              r'(?P<time>\d+:\d+:?\d*|0)\s+' \
                              r'(?P<state>[BCEFHMQRSTUWX])\s+' \
                              r'(?P<queue_name>\w+)'
 
-        self.user_q_search_prior = r'\s{0,2}' \
-                                   r'(?P<job_id>\d+)\s+' \
-                                   r'(?:[0-9]\.[0-9]+)\s+' \
-                                   r'(?:[\w.-]+)\s+' \
-                                   r'(?P<user>[\w.-]+)\s+' \
-                                   r'(?P<state>[a-z])\s+' \
-                                   r'(?:\d{2}/\d{2}/\d{2}|0)\s+' \
-                                   r'(?:\d+:\d+:\d*|0)\s+' \
-                                   r'(?P<queue_name>\w+@[\w.-]+)\s+' \
-                                   r'(?:\d+)\s+' \
-                                   r'(?:\w*)'
+
 
     def extract_qstat(self, orig_file):
         try:
@@ -44,17 +43,11 @@ class PBSStatExtractor(StatExtractor):
                 fin.readline()
                 line = fin.readline()
                 re_match_positions = ('job_id', 'user', 'state', 'queue_name')  # was: (1, 5, 7, 8), (1, 4, 5, 8)
-                try:  # first qstat line determines which format qstat follows.
-                    re_search = self.user_q_search
-                    qstat_values = self._process_qstat_line(re_search, line, re_match_positions)
-                    # unused: _job_nr, _ce_name, _name, _time_use = m.group(2), m.group(3), m.group(4), m.group(6)
-                except AttributeError:  # this means 'prior' exists in qstat, it's another format
-                    re_search = self.user_q_search_prior
-                    qstat_values = self._process_qstat_line(re_search, line, re_match_positions)
-                    # unused:  _prior, _name, _submit, _start_at, _queue_domain, _slots, _ja_taskID =
-                    # m.group(2), m.group(3), m.group(6), m.group(7), m.group(9), m.group(10), m.group(11)
-                finally:
-                    all_qstat_values.append(qstat_values)
+
+                re_search = self.user_q_search
+                qstat_values = self._process_qstat_line(re_search, line, re_match_positions)
+
+                all_qstat_values.append(qstat_values)
 
                 # hence the rest of the lines should follow either try's or except's same format
                 for line in fin:
